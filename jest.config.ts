c27f3de03c9df8bb710ee1d2bd8892270f53656e
rrc_w2025_jojanpreet_ <jjojanpreetkaur@rrc.ca>
2026-01-19 10:29:23 -0600

    test: configure Jest and Supertest for API testing

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/test/**/*.test.ts"],
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/server.ts"
+    ]
+};
